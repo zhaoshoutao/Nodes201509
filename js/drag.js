22752c3703e99a490c0cbc86acc68283ea37a83a
zhaoshoutao <690337924@qq.com>
2016-02-28 10:15:29 +0800

    第六周周末

---
+++ js/drag.js
@@ -0,0 +1,43 @@
+function down(e) {
+    this.strX = e.pageX;
+    this.strY = e.pageY;
+    this.strL = this.offsetLeft;
+    this.strT = this.offsetTop;
+    if (this.setCapture) {
+        this.setCapture();
+        zhufengEvent.on(this, "mousemove", move);
+        zhufengEvent.on(this, "mouseup", up);
+    } else {
+        this.MOVE = zhufengEvent.processThis(move, this);
+        this.UP = zhufengEvent.processThis(up, this);
+        zhufengEvent.on(document, "mousemove", this.MOVE);
+        zhufengEvent.on(document, "mouseup", this.UP);
+    }
+
+    zhufengEvent.fire.call(this, "zhufengDragStart", e);
+}
+
+function move(e) {
+    var curL = e.pageX - this.strX + this.strL;
+    var curT = e.pageY - this.strY + this.strT;
+    //var minL = 0, minT = 0, maxL = (document.documentElement.clientWidth || document.body.clientWidth) - this.offsetWidth, maxT = (document.documentElement.clientHeight || document.body.clientHeight) - this.offsetHeight;
+    //curL = curL <= minL ? minL : (curL >= maxL ? maxL : curL);
+    //curT = curT <= minT ? minT : (curT >= maxT ? maxT : curT);
+    this.style.left = curL + "px";
+    this.style.top = curT + "px";
+
+    zhufengEvent.fire.call(this, "zhufengDragMove", e);
+}
+
+function up(e) {
+    if (this.releaseCapture) {
+        this.releaseCapture();
+        zhufengEvent.off(this, "mousemove", move);
+        zhufengEvent.off(this, "mouseup", up);
+    } else {
+        zhufengEvent.off(document, "mousemove", this.MOVE);
+        zhufengEvent.off(document, "mouseup", this.UP);
+    }
+
+    zhufengEvent.fire.call(this, "zhufengDragEnd", e);
+}
